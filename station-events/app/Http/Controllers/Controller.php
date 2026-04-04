3705072667f44c64d7620e1d31a6810512e2be12
Ahmed Hesham <ahmed.heshamahmed@breadfast.com>
2026-04-04 15:40:39 +0200

    add common success reponse shared to other controllers for unify responses

--- station-events/app/Http/Controllers/Controller.php
+++ station-events/app/Http/Controllers/Controller.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
 abstract class Controller
 {
-    //
+    public function successResponse(mixed $data): JsonResponse
+    {
+        return response()->json($data, Response::HTTP_OK);
+    }
 }
